f23e685f0e49524b7c1b641ed20f7f5457769b9e
fd <dalirani.1373@gmail.com>
2024-06-16 02:22:39 -0400

    remove an extra stall in code

--- src/loopclosure.cpp
+++ src/loopclosure.cpp
@@ -869,8 +869,6 @@ namespace slam
             {
                 AddToProcessedKeyframes();
             }
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
         
         // Global pose graph optimization
